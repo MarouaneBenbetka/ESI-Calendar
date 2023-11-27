23004157b21f2a7d75aeaaa018f644be3466a166
Marouane <MarouaneBenbetka@gmail.com>
2023-11-27 20:22:43 +0100

    remove time zone

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ import { classes } from "./data/data";
 function App() {
 	const [room, setRoom] = useState(classes[0]);
 	return (
-		<section className="h-screen w-screen bg-gray-900 flex justify-center items-center flex-col overflow-y-auto">
-			<div className="flex flex-col items-center">
+		<section className="h-screen w-screen bg-gray-900 flex justify-center items-center flex-col overflow-y-auto pt-4">
+			<div className="flex flex-col items-center ">
 				<SearchBar setClass={setRoom} />
 				<iframe
-					src={`https://calendar.google.com/calendar/embed?src=${room.src}&showPrint=0&showCalendars=0&mode=WEEK`}
+					src={`https://calendar.google.com/calendar/embed?src=${room.src}&showTz=0&showPrint=0&showCalendars=0&mode=WEEK`}
 					className="hidden md:block bg-white border-0 rounded-xl overflow-hidden pt-3 mt-8 w-[90vw] h-[75vh] md:w-[800px] md:h-[500px]"
 				></iframe>
 				<iframe
-					src={`https://calendar.google.com/calendar/embed?src=${room.src}&showPrint=0&showTitle=1&showDate=0&showTabs=0&showCalendars=0&mode=DAY`}
-					className="block md:hidden bg-white border-0 rounded-xl overflow-hidden pt-3 mt-8 w-[90vw] h-[75vh] md:w-[800px] md:h-[500px]"
+					src={`https://calendar.google.com/calendar/embed?src=${room.src}&showTz=0&showPrint=0&showTitle=1&showDate=0&showTabs=0&showCalendars=0&mode=DAY`}
+					className="block md:hidden bg-white border-0 rounded-xl overflow-hidden pt-3 mt-8 w-[90vw] h-[70vh] md:w-[800px] md:h-[500px]"
 				></iframe>
 			</div>
 			<a
